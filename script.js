79518d7deb252856742aae47ead0050840dc8d34
SHEIKH SHAADI <sheikhshaadi137@gmail.com>
2024-09-26 01:05:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let vBtn = document.querySelectorAll('.v-btn');
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
+
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+})
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+})
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+})
+
+vBtn.forEach(btn =>{
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active')
+        btn.classList.add('active')
+        let src = btn.getAttribute('data-src')
+        document.querySelector('#video-slider').src = src
+    })
+})
+
+// let swiper = new Swiper(".review-slider", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: true,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints: {
+//         640: {
+//             slidesPreView: 1,
+//         },
+//         768: {
+//             slidesPreView: 2,
+//         },
+//         1024: {
+//             slidesPreView: 3,
+//         },
+//     },
+// });
+
+
+
+
+
+
